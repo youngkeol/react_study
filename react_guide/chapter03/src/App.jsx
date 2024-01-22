ae320b75ec359a19a4e6d323862d03f7e87a48c6
younggori <younggori@MacBookPro.local>
2024-01-23 01:24:01 +0900

    [2024.01.23] chapter03
    chapter03

---
+++ react_guide/chapter03/src/App.jsx
@@ -0,0 +1,78 @@
+import Header from './components/Header/Header';
+import CoreConcept from './components/CoreConcept';
+
+
+import { CORE_CONCEPTS } from './data';
+import Tabutton from './components/Tabutton';
+import { useState } from 'react';
+import {EXAMPLES} from './data.js';
+
+
+function App() {
+    const [selectedTopic, setSelectedTopic] =  useState();
+    
+    function handleClick(selectedButton){
+        setSelectedTopic(selectedButton);
+    }
+
+    let tabContent =  (<p>Please select a topic.</p>);
+    if(selectedTopic) {
+        tabContent = (<
+            div id="tab-content">
+                <h3>{EXAMPLES[selectedTopic].title}</h3>
+                <p>{EXAMPLES[selectedTopic].description}</p>
+                <pre>
+                    <code>{EXAMPLES[selectedTopic].code}</code>
+                </pre>
+            </div>
+    )}
+
+
+
+    return (
+        <div>
+            <Header />
+            <main>
+                <section id="core-concepts">
+                    <h2>Time to get ÷ started!</h2>
+                    <ul>
+                        {CORE_CONCEPTS.map((conceptItem) => (
+                            <CoreConcept
+                                key = {conceptItem.image}
+                                title={conceptItem.title}
+                                description={conceptItem.description}
+                                image={conceptItem.image}
+                            />
+
+                        ))}
+                    </ul>
+                </section>
+                <section id="examples">
+                    <h2>Examples</h2>
+                    <menu>
+                        <Tabutton 
+                            isSelected={selectedTopic =='components'} 
+                            onSelect={(e) => handleClick('components')}>Compoents
+                        </Tabutton>
+                        <Tabutton 
+                            isSelected={selectedTopic =='jsx'} 
+                            onSelect={(e) => handleClick('jsx')}>Jsx
+                        </Tabutton>
+                        <Tabutton 
+                            isSelected={selectedTopic =='props'} 
+                            onSelect={(e) => handleClick('props')}>Props
+                        </Tabutton>
+                        <Tabutton 
+                            isSelected={selectedTopic =='state'} 
+                            onSelect={(e) => handleClick('state')}>States
+                        </Tabutton>
+                    </menu>
+                    
+                    {tabContent}
+                </section>
+            </main>
+        </div>
+    );
+}
+
+export default App;
