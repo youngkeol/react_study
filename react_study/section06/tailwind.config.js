c51bb7e3951fc5ad68421b7526b02ec1c80415d0
younggori <hyk1272@gamil.com>
2024-06-16 19:35:02 +0900

    [2024.06.16]  컴포넌트 스타일링, 앱 디버깅
    컴포넌트 스타일링, 앱 디버깅

---
+++ react_study/section06/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./src/**/*.{html,js,ts,tsx,jsx}"],
+    theme: {
+      extend: {
+        fontFamily : {
+            title: ['"Pacifico"', 'cursive']
+        }
+      },
+    },
+    plugins: [],
+  }
