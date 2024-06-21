b61ed559d23b52fba6ce8663385e313913a90b6a
younggori <hyk1272@gamil.com>
2024-06-21 17:25:08 +0900

    [2024.06.21] 섹션 10 - context api, reducer
    context api, reducer

---
+++ react_study/section10/src/components/Shop.jsx
@@ -0,0 +1,14 @@
+
+import Product from './Product.jsx';
+
+export default function Shop({ children }) {
+  return (
+    <section id="shop">
+      <h2>Elegant Clothing For Everyone</h2>
+
+      <ul id="products">
+        {children}
+      </ul>
+    </section>
+  );
+}
